3ab315f29bf3836004802c368d882f0e274d0956
Sowmya <sowmya.sri.37.2@mountblue.tech>
2026-08-12 09:38:50 +0530

    Made board card to clickable and navigate to their individual board page

--- src/components/BoardCard.jsx
+++ src/components/BoardCard.jsx
@@ -1,9 +1,22 @@
+import { useNavigate } from "react-router-dom";
+
 function BoardCard({ board }) {
+
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(`/boards/${board.id}`);
+  }
   return (
-    <article className="flex h-50 mt-5 cursor-pointer items-end rounded-lg border border-gray-200 bg-white p-4 shadow-md transition-shadow duration-200 hover:shadow-lg">
+    <article
+      onClick={handleClick}
+      className="flex h-50 mt-5 cursor-pointer items-end rounded-lg border border-gray-200 bg-white p-4 shadow-md
+      transition-shadow duration-200 hover:shadow-lg">
+
       <h2 className="text-lg font-semibold text-gray-800">
         {board.name}
       </h2>
+
     </article>
   )
 }
