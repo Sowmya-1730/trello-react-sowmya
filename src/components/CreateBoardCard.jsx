7b0dff49e2f503755ee094a665489b069eadfe81
Sowmya <sowmya.sri.37.2@mountblue.tech>
2026-08-12 12:35:04 +0530

    Made the board card to rename when we click on name by double click for Rename operation

--- src/components/CreateBoardCard.jsx
+++ src/components/CreateBoardCard.jsx
@@ -8,7 +8,7 @@ function CreateBoardCard({ onClick }) {
       className="flex h-50 mt-5 cursor-pointer border-dashed border-2 items-center justify-center rounded-lg  border-gray-300 bg-gray-80 p-4 shadow-md transition-shadow duration-200 hover:shadow-lg"
     >
       <span className="text-lg font-semibold">
-        + Create Board
+         Create new board
       </span>
     </button>
   )
